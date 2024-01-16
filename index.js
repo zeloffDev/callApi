e0e09629c3af5d009b0896eb45769d4038d3a4dd
huytuan <huytuan2200@gmail.com>
2024-01-16 21:32:50 +0700

    call yoursel

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const axios = require("axios");
+const express = require("express");
+const app = express();
 const http = require("http");
 require("dotenv").config();
-const server = http.createServer();
+const server = http.createServer(app);
 server.listen(3002, () => {
   console.log("SERVER RUNNING");
 });
 const apiUrl = process.env.API_URL;
+const testApi = process.env.TEST_API_URL;
 const time = process.env.TIME;
+const day = process.env.DAY;
+
+app.use("/", (req, res) => {
+  res.status(200).json("Test Request");
+});
 
 const callApi = async () => {
   try {
     const response = await axios.get(apiUrl);
-    console.log("API Response:", response.data);
+    const testRequest = await axios.get(testApi);
+
+    console.log("API Response:", response.data, testRequest.data);
   } catch (error) {
     console.error("API Error:", error.message);
   }
@@ -19,7 +29,6 @@ const callApi = async () => {
 
 const intervalId = setInterval(callApi, Number(time) * 60 * 1000);
 
-// setTimeout(() => {
-//   clearInterval(intervalId);
-//   console.log("Lập lịch đã dừng sau 24 giờ.");
-// }, 24 * 60 * 60 * 1000);
+setTimeout(() => {
+  clearInterval(intervalId);
+}, Number(day) * 24 * 60 * 60 * 1000);
